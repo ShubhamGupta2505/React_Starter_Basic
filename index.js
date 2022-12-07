74f8645db85bb8d8599dac95c8003ec16e6bb777
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-07 23:57:15 +0530

    JSX

--- index.js
+++ index.js
@@ -50,10 +50,15 @@
 //         </div>,
 //     document.getElementById("root"))
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is imperative way to program "
-h1.className = "header"
-console.log(h1)
-const element = <h1 className="header" >This is JSX </h1>
-console.log(element)
-ReactDOM.render(element,document.getElementById("root"))
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is imperative way to program "
+// h1.className = "header"
+// console.log(h1)
+// const element = <h1 className="header" >This is JSX </h1>
+//console.log(element)
+ReactDOM.render(
+    <div>
+    <h1 className="header" >This is JSX </h1>
+    <p>This is paragraph </p>
+    </div>,
+    document.getElementById("root"))
