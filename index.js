37b0c3dd00bbf1982378bdc1cc6a536884c7b432
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-07 23:51:52 +0530

    JSX

--- index.js
+++ index.js
@@ -53,4 +53,7 @@
 const h1 = document.createElement("h1")
 h1.textContent = "This is imperative way to program "
 h1.className = "header"
-document.getElementById("root").append(h1)
+console.log(h1)
+const element = <h1 className="header" >This is JSX </h1>
+console.log(element)
+ReactDOM.render(element,document.getElementById("root"))
