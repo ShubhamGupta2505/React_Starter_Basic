e4d1194bc28b966ba39838a52be597e5455705a8
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-08 19:00:49 +0530

    Basic static info page

---
+++ project1.js
@@ -0,0 +1,14 @@
+const page1 = (
+    <div>
+        <h1>Fun facts About React</h1>
+        <ul>
+            <li>Was First released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Power thousands os enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page1,document.getElementById("root"))
