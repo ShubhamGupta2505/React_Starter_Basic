92c52788ff82ad5dff550ced1acfbcf71d4df741
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-09 19:27:37 +0530

    Parent Child Component

--- project1.js
+++ project1.js
@@ -26,22 +26,40 @@
 //     </div>
 //     )
 // }
-
-function Page() {
+function Header() {
     return (
-        <div>
-            <header>
+       <div>
+           <header>
                 <nav>
                     <img src ="./react-logo.png" width="80px"/>
                 </nav>
             </header>
-            <h1>Reason I am excited to learn React  </h1>
+       </div> 
+    )
+}
+function Footer() {
+    return (
+        <footer>2022 XYZ development. All rights reserved .</footer>
+    )
+}
+function MainContent(){
+    return(
+        <div>
+            <h1>Reason I am excited to learn React : </h1>
             <ol>
                 <li>It is a popular library </li>
-                <li>It is fun to learn</li>
+                <li>It is fun to learn and creative.</li>
                 <li>It is more likely to get a job as developer .</li>
             </ol>
-            <footer>2022 XYZ development. All rights reserved .</footer>
+        </div>
+    )
+}
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent/>
+            <Footer />
         </div>
     )
 }
