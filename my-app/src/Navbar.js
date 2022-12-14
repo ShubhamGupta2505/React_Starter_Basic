169d4d2f6143912796e0e466e7b89b66b98b48ed
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-12-14 22:28:28 +0530

    Basic info React Site

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <h1>Navbar Components</h1>
+        <nav>
+            <img src="./logo192.png" className="nav--icon"/>
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
